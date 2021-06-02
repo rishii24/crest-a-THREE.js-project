1851f05023a2d355d6586d8c313b09c58d520ac9
rishii24 <24rishi02@gmail.com>
2021-06-02 21:34:18 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,137 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+//Texture Loader
+const loader = new THREE.TextureLoader()
+const height = loader.load('./height.png')
+const texture = loader.load('./texture.jpg')
+const alpha = loader.load('./alpha.png')
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
+
+// Materials
+
+const material = new THREE.MeshStandardMaterial({
+    color: 'gray',
+    map: texture,
+    displacementMap: height,
+    displacementScale: 0.9, //height adjustment
+    alphaMap: alpha,
+    transparent: true, //to make alpha work
+    depthTest: false //false for removing blind effect from sides
+})
+
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane)
+plane.rotation.x = 181
+
+// Lights
+
+const pointLight = new THREE.PointLight(0x00b3ff, 3)
+pointLight.position.x = .2
+pointLight.position.y = 10
+pointLight.position.z = 4.4
+scene.add(pointLight)
+
+
+const col = {
+    color: '#00b3ff'
+}
+
+// Updating color with the help of GUI
+
+// gui.addColor(col, 'color').onChange(() => {
+//     pointLight.color.set(col.color)
+// })
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth * 7,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth * .7
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha:true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+document.addEventListener('mousemove', animateTerrain)
+
+let mouseY = 0
+
+function animateTerrain(event) {
+    mouseY = event.clientY
+}
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    //For the rotation of the plane
+
+    plane.rotation.z = .5 * elapsedTime
+    plane.material.displacementScale = 0.5 + mouseY * 0.0009
+    // Update Orbital Controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
